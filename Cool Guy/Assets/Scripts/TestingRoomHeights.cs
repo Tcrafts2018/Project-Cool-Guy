4c942b3eb019fae5853d2883863f1a0ae1d9300c
Tcrafts2018 <49214464+Tcrafts2018@users.noreply.github.com>
2020-09-11 12:27:29 -0400

    Room_height testing

---
+++ Cool Guy/Assets/Scripts/TestingRoomHeights.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingRoomHeights : MonoBehaviour
+{
+    public GameObject room;
+    private BoxCollider roomCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        roomCollider = room.GetComponent<BoxCollider>();
+        Debug.Log(roomCollider.bounds.size.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
