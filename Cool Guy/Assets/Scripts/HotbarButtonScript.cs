a700b225cbd18ff0d153d02d1632e890f39045b3
Tcrafts2018 <49214464+Tcrafts2018@users.noreply.github.com>
2020-10-02 23:46:32 -0400

    Revert "Revert "Revert "Revert "wooo""""
    This reverts commit c8cbc37af6090649204147f2903efe901433ed61.

---
+++ Cool Guy/Assets/Scripts/HotbarButtonScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotbarButtonScript : MonoBehaviour
+{
+    bool is1Equipped = true;
+    bool is2Equipped = false;
+    bool is3Equipped = false;
+    bool is4Equipped = false;
+    public GameObject coolBullet; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        coolBullet = Resources.Load("Prefabs/Bullet/Bullet") as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log("1 is Pressed");
+            is1Equipped = true;
+            is2Equipped = false;
+            is3Equipped = false;
+            is4Equipped = false;
+            coolBullet = Resources.Load("Prefabs/Bullet/Bullet") as GameObject;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Debug.Log("2 is Pressed");
+            is1Equipped = false;
+            is2Equipped = true;
+            is3Equipped = false;
+            is4Equipped = false;
+            coolBullet = Resources.Load("Prefabs/Bullet/Bullet 2") as GameObject;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Debug.Log("3 is Pressed");
+            is1Equipped = false;
+            is2Equipped = false;
+            is3Equipped = true;
+            is4Equipped = false;
+            coolBullet = Resources.Load("Prefabs/Bullet/Bullet 3") as GameObject;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Debug.Log("4 is Pressed");
+            is1Equipped = false;
+            is2Equipped = false;
+            is3Equipped = false;
+            is4Equipped = true;
+        }
+    }
+}
